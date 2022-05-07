e3aec3bee3e57b047d183d2778ce70c6c4a5b785
Elie-José Maalouf <eliejoseejm@gmail.com>
2022-05-07 14:01:18 +0300

    Initial UI setup and repo implementation

---
+++ app/src/main/java/com/example/yassirmovies/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.yassirmovies.ui
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.yassirmovies.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getMovies()
+    }
+}
